0ffe96699bc9484b682130f7be2122c80047c6e5
Simon Luder <simon.luder@students.fhnw.ch>
2025-03-21 15:31:57 +0100

    Add poleno dataset

---
+++ pollen_datasets/poleno.py
@@ -0,0 +1,247 @@
+import os
+import torch
+import pickle
+import numpy as np
+import pandas as pd
+from PIL import Image
+from pathlib import Path
+from typing import Optional
+
+class BaseHolographyImageFolder(torch.utils.data.Dataset):
+    
+    def __init__(self, root, transform=None, labels: Optional[str]=None, config: Optional[str]=None, conditioning=None, verbose: bool=False):
+        self.root = root
+        self.transform = transform
+        self.config = config
+        self.labels = labels
+        self.cond_imgs = None
+        self.tabular_features = None
+        self.class_labels = None
+        self.samples = None
+
+        if labels is not None and not isinstance(labels, str):
+            raise TypeError("Expected a string or None, got type: {}".format(type(labels).__name__))
+        
+        if (labels is not None) and os.path.exists(labels):
+
+            if labels.endswith(".csv"):
+                if verbose:
+                    print("Loading dataset from csv")
+                self.load_annotations_from_csv()
+
+            if labels.endswith(".pkl"):
+                if verbose:
+                    print("Loading dataset from pickle")
+                self.load_annotations_from_pickle()
+
+        if self.samples is None:
+            if verbose:
+                print("Search for image files in root and child folders. This might take a while...")
+            self.search_images_in_folder()
+
+
+    def load_annotations_from_csv(self):
+        raise NotImplementedError("Method not implemented for this dataset.")
+
+
+    def load_annotations_from_pickle(self):
+        raise NotImplementedError("Method not implemented for this dataset.")
+
+
+    def search_images_in_folder(self):
+        raise NotImplementedError("Method not implemented for this dataset.")
+
+
+    def __len__(self):
+        return len(self.samples)
+
+
+class HolographyImageFolder(BaseHolographyImageFolder):
+    
+    def __init__(self, root, transform=None, labels=None, config=None, conditioning=None, verbose=False):
+        super().__init__(root, transform, labels, config, conditioning, verbose) 
+
+    def search_images_in_folder(self):
+        
+        # Search the root dir if no valid labels file is given
+        if self.labels is None or not os.path.exists(self.labels):
+            search_root_dir = True
+        else: 
+            search_root_dir = False
+
+        foldername_to_id = dict()
+        index = 0
+        if search_root_dir:
+            # Get all images in root and subdirs of root
+            self.samples = []
+            for dirpath, _, filenames in os.walk(self.root):
+                for filename in filenames:
+                    if filename.lower().endswith(('.png', '.jpg', '.jpeg')):
+                        img_path = os.path.join(dirpath, filename)
+                        folder_name = os.path.basename(os.path.dirname(img_path))
+                        if folder_name not in foldername_to_id.keys():
+                            foldername_to_id[folder_name] = index
+                            index += 1
+                        self.samples.append((img_path, foldername_to_id[folder_name], filename))
+
+            # Save the searched samples as pickle file
+            if self.labels is not None and self.labels.lower().endswith((".pickle", ".pkl")):
+                Path(os.path.dirname(os.path.split(self.labels)[-1])).mkdir(parents=True, exist_ok=True)
+                with open(self.labels, 'wb') as f:
+                    pickle.dump(self.samples, f)
+
+            # Save the searched samples as csv file
+            if self.labels is not None and self.labels.lower().endswith(".csv"):
+                Path(os.path.dirname(os.path.split(self.labels)[-1])).mkdir(parents=True, exist_ok=True)
+                pd.DataFrame(self.samples).to_csv(self.labels, index=False, header=False)
+        
+
+    def load_annotations_from_csv(self):
+        df = pd.read_csv(self.labels)
+        class_cond_colunmn = self.config.get("classes", None)
+        feature_columns = self.config.get("features", None)
+        cond_image_colunmn = self.config.get("cond_img_path", None)
+        filename_column = self.config["filenames"]
+        image_folder = self.config["img_path"]
+
+        filenames = [filename for filename in list(df[filename_column])]
+        img_paths = [os.path.join(image_folder, filename) for filename in filenames]
+        self.samples = list(zip(img_paths, filenames))
+
+        # class features for conditioning
+        if class_cond_colunmn is not None:
+            self.class_labels = df[class_cond_colunmn].values.tolist()
+        else:
+            self.class_labels = None
+
+        # tabular features for conditioning
+        if feature_columns is not None:
+            self.tabular_features = df[feature_columns].values.tolist()
+        else:
+            self.tabular_features = None
+            
+        # images for conditioning
+        if cond_image_colunmn is not None:
+            self.cond_imgs = df[cond_image_colunmn].values.tolist()
+        else:
+            self.cond_imgs = None
+        
+
+    def load_annotations_from_pickle(self):
+        # load samples from pickle file if exists
+        with open(self.labels, 'rb') as f:
+            self.samples = pickle.load(f)
+
+
+    def __getitem__(self, idx):
+
+        # get image
+        img_path, filename = self.samples[idx]
+        img = Image.open(img_path)
+
+        if img.mode == 'I':
+            img = img.convert('I;16') 
+            img = (np.array(img) / 256).astype('uint8')
+
+        elif img.mode == 'L':
+            img = np.array(img).astype('uint8')
+
+        if self.transform:
+            img = self.transform(img)   
+
+        # get conditioning
+        condition = dict()
+
+        if self.class_labels:
+            condition["class"] = self.class_labels[idx]
+
+        if self.tabular_features:
+            tab_cond = self.tabular_features[idx]
+            condition["tabular"] = torch.tensor(tab_cond)
+
+        if self.cond_imgs:
+            # cond_img_path = self.cond_imgs[idx]
+
+            # # temporarly
+            # cond_img = Image.open(cond_img_path)
+
+            # if cond_img.mode == 'I':
+            #     cond_img = cond_img.convert('I;16')
+                
+            # cond_img = (np.array(cond_img) / 256).astype('uint8')
+
+            # # TODO Open image as tensor and preprocess
+ 
+            condition["image"] = img
+
+        return img, condition, filename
+
+
+class PairwiseHolographyImageFolder(BaseHolographyImageFolder):
+    
+    def __init__(self, root, transform=None, labels=None, config=None, conditioning=None, verbose=False):
+        super().__init__(root, transform, labels, config, conditioning, verbose) 
+
+
+    def load_annotations_from_csv(self):
+        
+        df = pd.read_csv(self.labels)
+        class_cond_colunmn = self.config.get("classes", None)
+        feature_columns = self.config.get("features", None)
+        cond_image_colunmn = self.config.get("cond_img_path", None)
+        filename_column = self.config["filenames"]
+        image_folder = self.config["img_path"]
+        event_id_column = "event_id"
+
+        # Group by event_id
+        grouped = df.groupby(event_id_column)
+        self.samples = []
+        self.class_labels = []
+        self.tabular_features = []
+        self.cond_imgs = []
+
+        for event_id, group in grouped:
+            if len(group) != 2:
+                raise ValueError(f"Event ID {event_id} does not have exactly two rows.")
+            
+            filenames = group[filename_column].tolist()
+            img_paths = [os.path.join(image_folder, filename) for filename in filenames]
+            self.samples.append((img_paths, filenames))
+
+            if class_cond_colunmn is not None:
+                self.class_labels.append(group[class_cond_colunmn].tolist())
+            if feature_columns is not None:
+                self.tabular_features.append(group[feature_columns].values.tolist())
+            if cond_image_colunmn is not None:
+                self.cond_imgs.append(group[cond_image_colunmn].tolist())
+
+
+    def __getitem__(self, idx):
+        img_paths, filenames = self.samples[idx]
+        imgs = [Image.open(img_path) for img_path in img_paths]
+
+        for i, img in enumerate(imgs):
+            if img.mode == 'I':
+                img = img.convert('I;16') 
+                img = (np.array(img) / 256).astype('uint8')
+            elif img.mode == 'L':
+                img = np.array(img).astype('uint8')
+
+            if self.transform:
+                imgs[i] = self.transform(img)   
+
+        # get conditioning
+        condition = dict()
+
+        if self.class_labels:
+            condition["class"] = self.class_labels[idx]
+
+        if self.tabular_features:
+            tab_cond = self.tabular_features[idx]
+            condition["tabular"] = torch.tensor(tab_cond)
+
+        if self.cond_imgs:
+            condition["image"] = imgs
+
+        return imgs, condition, filenames
+
