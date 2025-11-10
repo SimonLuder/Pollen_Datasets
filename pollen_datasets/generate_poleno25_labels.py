cf1a4a1d8cfdc20f803d5b829ef5654ffd8dafa5
Simon Luder <simon.luder@students.fhnw.ch>
2025-11-10 15:32:08 +0100

    Add image annotation labels

--- pollen_datasets/generate_poleno25_labels.py
+++ pollen_datasets/generate_poleno25_labels.py
@@ -1,5 +1,8 @@
 import os
+import re
 import sys
+import json
+import logging
 import argparse
 import pandas as pd
 from datetime import datetime
@@ -24,14 +27,15 @@ def get_datasets_as_df(folder):
         ]
 
         if not csv_files:
-            print("No CSV files found.")
+            logging.info("No CSV files found.")
         else:
-            print(f"Found {len(csv_files)} CSV files. Combining...")
-
+            logging.info(f"Found {len(csv_files)} CSV files. Combining...")
+            
             # Read and combine all CSVs
             df = pd.concat((pd.read_csv(f) for f in csv_files), ignore_index=True)
             return df
         
+
 def has_columns(df, required_cols):
     """
     Checks if a DataFrame contains all specified columns.
@@ -46,15 +50,60 @@ def has_columns(df, required_cols):
     """
     missing = [col for col in required_cols if col not in df.columns]
     return len(missing) == 0, missing
-        
 
-def generate_labels(images_root, labels_folder, filename, force_recalc):
 
-    feature_cols = [
+
+# -------------------- Further data setup functions --------------------
+
+def image_nr_from_rec_path(rec_path):
+    """Get the image number from the rec_path"""
+    pattern = re.compile(r'image_pairs\.\d+\.(\d+)\.rec_mag')
+    return int(pattern.search(rec_path).group(1)) if pattern.search(rec_path) else None
+
+
+def species_from_dataset_id(dataset_id, id_to_species):
+    """Get the species from the dataset_id"""
+    return id_to_species.get(dataset_id, None)
+
+
+def genus_from_species(species):
+    """Get the genus from the species"""
+    return species.split(" ")[0] if species is not None else None
+
+
+def add_annotations(df, dataset_ids_file):
+    """Add rest of annotation columns to the dataset DataFrame."""
+    # dataset_id to species mapper
+    with open(dataset_ids_file, "r") as f:
+        id_to_species = json.load(f)
+    # Additional feature columns
+    df["species"]  = df["dataset_id"].apply(species_from_dataset_id, id_to_species=id_to_species)
+    df["genus"]    = df["species"].apply(genus_from_species)
+    df["image_nr"] = df["rec_path"].apply(image_nr_from_rec_path)
+    return df
+
+
+def generate_labels(images_root, labels_folder, out_file, dataset_ids_file, force_recalc):
+
+    regionprops_cols = [
         "area","bbox_area","convex_area","eccentricity","equivalent_diameter","feret_diameter_max",
         "major_axis_length","minor_axis_length","max_intensity","min_intensity","mean_intensity",
         "orientation","perimeter","perimeter_crofton","solidity"
         ]
+    
+    annotations_cols = [
+        "image_nr", "species", "genus"
+        ]
+    
+    # Configure logging
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s [%(levelname)s] %(message)s',
+        handlers=[
+            logging.FileHandler("generate_poleno25_labels.log", mode='a', encoding='utf-8'),
+            logging.StreamHandler(sys.stdout)
+        ]
+    )
 
     # Get current databasis
     df = get_datasets_as_df(labels_folder)
@@ -62,9 +111,8 @@ def generate_labels(images_root, labels_folder, filename, force_recalc):
         already_searched = list(set(df["dataset_id"]))
     else:
         already_searched = []
-    
-    ignore = already_searched 
 
+    logging.info(f"Searching in {images_root}, ignore folders: {already_searched}")
     # Search missing images
     setup = DataSetup()
     setup.search_images_in_folder(
@@ -73,35 +121,59 @@ def generate_labels(images_root, labels_folder, filename, force_recalc):
     )
     timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     setup.save_as_csv(os.path.join(labels_folder, f"collection_{timestamp}.csv"))
+    logging.info("Done")
 
     # Get all csv files
     all_csv = [os.path.join(labels_folder, file) for file in os.listdir(labels_folder) if file.endswith(".csv")]
-    print(all_csv)
+    logging.info(f"Found csv's: {all_csv}")
 
     # Itterate over all csv files separately
     for csv in all_csv:
 
         df = pd.read_csv(csv)
-        complete, _ = has_columns(df, feature_cols)
+        regionprops_complete, _ = has_columns(df, regionprops_cols)
+        annotations_complete, _ = has_columns(df, annotations_cols)
 
-        if force_recalc or not complete:
+        if force_recalc or not regionprops_complete:
+            
             # Recalculate features for all images in the dataframe
-            computed_data_full_recalc = recalculate_holographic_features(df, images_root)
-            # Save the dataframe
-            computed_data_full_recalc.to_csv(os.path.join(labels_folder, f"collection_{timestamp}.csv"), index=False)
+            logging.info(f"Recalculate regionprops for {csv}")
+            df = recalculate_holographic_features(df, images_root)
 
-    # Get updated databasis
+            # Save the dataframe
+            df.to_csv(csv, index=False)
+
+        if force_recalc or not annotations_complete:
+
+            # Add image annotation labels
+            logging.info(f"Adding annotations to {csv}")
+            df = add_annotations(df, dataset_ids_file)
+
+            # Save the dataframe
+            df.to_csv(csv, index=False)
+        
+    # Get updated databasis and save as single csv
     df = get_datasets_as_df(labels_folder)
+    os.makedirs(os.path.basename(out_file), exist_ok=True)
+    df.to_csv(out_file, index=False)
+    logging.info(f"Created final csv for {out_file}")
 
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Arguments for resnet training')
-    parser.add_argument('--labels', default="./data/processed/poleno_25/", type=str)
+    parser.add_argument('--labels', default="./data/processed/poleno_25/temp/", type=str)
     parser.add_argument('--root', default='Z:/marvel/marvel-fhnw/data/Poleno_25', type=str)
-    parser.add_argument('--filename', default='poleno_25_labels.csv', type=str)
-    parser.add_argument('--force_recalc', default=True, type=bool)
+    parser.add_argument('--filename', default='data/processed/poleno_25/poleno_25_labels.csv', type=str)
+    parser.add_argument('--force_recalc', default=False, type=bool)
+    parser.add_argument('--dataset_ids', default="Z:\marvel\marvel-fhnw\data\Poleno_25\dataset_ids.json", type=str)
     args = parser.parse_args()
 
-    generate_labels(args.root, args.labels, args.filename, args.force_recalc)
+    generate_labels(
+        args.root, 
+        labels_folder=args.labels, 
+        out_file=args.filename, 
+        dataset_ids_file=args.dataset_ids, 
+        force_recalc=args.force_recalc, 
+        )
 
