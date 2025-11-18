d5d8a7e790064b42468940807bf1456d97d0eda4
Simon Luder <simon.luder@students.fhnw.ch>
2025-11-18 15:12:22 +0100

    Make HolographyImageFolder modular

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Pollen Datasets
 
-This reporitory contains code to access data from the SwissPoleno project. 
+This repository contains code to access data from the SwissPoleno project. 
 
 *Note: This only works if you have the correct access rights. If you don't know what this dataset is, you probably don't have access rights.*
 
@@ -11,12 +11,16 @@ pip install git+https://github.com/SimonLuder/Pollen_Datasets.git
 ```
 
 **How to use:**
-```python
-from pollen_datasets.poleno import PairwiseHolographyImageFolder
 
-dataset = PairwiseHolographyImageFolder(
+Single images
+
+```python
+from pollen_datasets.poleno import HolographyImageFolder
+
+dataset = HolographyImageFolder(
     root="YourImagesRootFolder",
-    transform=transform, 
     labels="YourLabelsFilePath.csv"
+    transform=transform, 
+    config=config,
 )
 ```
--- pollen_datasets/poleno.py
+++ pollen_datasets/poleno.py
@@ -7,11 +7,11 @@ import numpy as np
 import pandas as pd
 from PIL import Image
 from pathlib import Path
-from typing import Optional
+from typing import Optional, Any
 
 class BaseHolographyImageFolder(torch.utils.data.Dataset):
     
-    def __init__(self, root, transform=None, labels: Optional[str]=None, config=None, conditioning=None, verbose: bool=False):
+    def __init__(self, root, transform=None, labels: Optional[str]=None, config=None, verbose: bool=False):
         self.root = root
         self.transform = transform
         self.config = config
@@ -26,6 +26,7 @@ class BaseHolographyImageFolder(torch.utils.data.Dataset):
         
         if (labels is not None):
             if os.path.exists(labels):
+
                 if labels.endswith(".csv"):
                     if verbose:
                         print("Loading dataset from csv")
@@ -34,14 +35,10 @@ class BaseHolographyImageFolder(torch.utils.data.Dataset):
                 if labels.endswith(".pkl"):
                     if verbose:
                         print("Loading dataset from pickle")
-                    self.load_annotations_from_pickle()
+                    self.load_annotations_from_pickle()       
+
             else:
                 raise FileNotFoundError(f"Labels file {labels} does not exist.")
-
-        if self.samples is None:
-            if verbose:
-                print("Search for image files in root and child folders. This might take a while...")
-            self.search_images_in_folder()
 
 
     def load_annotations_from_csv(self):
@@ -70,125 +67,74 @@ class BaseHolographyImageFolder(torch.utils.data.Dataset):
             # 8-bit grayscale, scale from [0, 255] to [0, 1]
             img = np.array(img).astype(np.float32) / 255.0
 
-        if self.transform:
-            img = self.transform(img)
-
         return img
 
 
 class HolographyImageFolder(BaseHolographyImageFolder):
-    
-    def __init__(self, root, transform=None, labels=None, config=None, conditioning=None, verbose=False):
-        super().__init__(root, transform, labels, config, conditioning, verbose) 
 
-    def search_images_in_folder(self):
-        
-        # Search the root dir if no valid labels file is given
-        if self.labels is None or not os.path.exists(self.labels):
-            search_root_dir = True
-        else: 
-            search_root_dir = False
-
-        foldername_to_id = dict()
-        index = 0
-        if search_root_dir:
-            # Get all images in root and subdirs of root
-            self.samples = []
-            for dirpath, _, filenames in os.walk(self.root):
-                for filename in filenames:
-                    if filename.lower().endswith(('.png', '.jpg', '.jpeg')):
-                        img_path = os.path.join(dirpath, filename)
-                        relative_path = os.path.relpath(img_path, self.root)
-                        folder_name = os.path.basename(os.path.dirname(img_path))
-                        if folder_name not in foldername_to_id.keys():
-                            foldername_to_id[folder_name] = index
-                            index += 1
-                        # self.samples.append((relative_path, filename, foldername_to_id[folder_name]))
-                        self.samples.append((relative_path, filename))
-
-            # Save the searched samples as pickle file
-            if self.labels is not None and self.labels.lower().endswith((".pickle", ".pkl")):
-                Path(os.path.dirname(os.path.split(self.labels)[-1])).mkdir(parents=True, exist_ok=True)
-                with open(self.labels, 'wb') as f:
-                    pickle.dump(self.samples, f)
-
-            # Save the searched samples as csv file
-            if self.labels is not None and self.labels.lower().endswith(".csv"):
-                Path(os.path.dirname(os.path.split(self.labels)[-1])).mkdir(parents=True, exist_ok=True)
-                pd.DataFrame(self.samples).to_csv(self.labels, index=False, header=["img_path", "filename"])
-        
+    def __init__(self, root, transform=None, labels=None, config=None, verbose=False):
+        """
+        Here, `config` is the full config dict or at least:
+          {
+            "dataset": ...,
+            "conditioning": ...
+          }
+        """
+        self.full_config = config
+        self.dataset_cfg = config["dataset"]
+        self.cond_cfg = config.get("conditioning", {})
+        super().__init__(root, transform, labels, config, verbose)
 
     def load_annotations_from_csv(self):
         df = pd.read_csv(self.labels)
 
-        if self.config is not None:
-            # Get abbreviation from config if they exists
-            filename_column = self.config.get("filenames", "filename")
-            image_folder = self.config.get("img_path", "img_path")
-            class_cond_colunmn = self.config.get("classes", None)
-            feature_columns = self.config.get("features", None)
-            cond_image_colunmn = self.config.get("cond_img_path", None)
-        else:
-            # Default names
-            filename_column = "filename"
-            image_folder = "img_path"
-            class_cond_colunmn = None
-            feature_columns = None
-            cond_image_colunmn = None
+        # image path + filename like before
+        filename_column = self.dataset_cfg.get("filenames", "filename")
+        image_folder_col = self.dataset_cfg.get("img_path", "img_path")
+        self.samples = list(zip(df[image_folder_col], df[filename_column]))
 
-        self.samples = list(zip(df[image_folder], df[filename_column]))
+        # generic condition storage
+        self.conditions: dict[str, list] = {}
 
-        # class features for conditioning
-        if class_cond_colunmn is not None:
-            self.class_labels = df[class_cond_colunmn].values.tolist()
-        else:
-            self.class_labels = None
+        enabled = self.cond_cfg.get("enabled", "") # enabled encoders
+        enabled_names = [c for c in enabled.replace(" ", "").split("+") if c]
 
-        # tabular features for conditioning
-        if feature_columns is not None:
-            self.tabular_features = df[feature_columns].values.tolist()
-        else:
-            self.tabular_features = None
-            
-        # images for conditioning
-        if cond_image_colunmn is not None:
-            self.cond_imgs = df[cond_image_colunmn].values.tolist()
-        else:
-            self.cond_imgs = None
-        
+        enc_cfgs = self.cond_cfg.get("encoders", {})
 
-    def load_annotations_from_pickle(self):
-        # load samples from pickle file if exists
-        with open(self.labels, 'rb') as f:
-            self.samples = pickle.load(f)
+        for name in enabled_names:
+            if name not in enc_cfgs:
+                raise KeyError(f"Condition '{name}' enabled but not defined in conditioning.encoders")
+            cfg = enc_cfgs[name]
+            cols = cfg["use_columns"]
+            if isinstance(cols, str):
+                cols = [cols]
 
+            # For now, just store raw values; convert to tensors in __getitem__
+            self.conditions[name] = df[cols].values.tolist()
 
     def __getitem__(self, idx):
-
-        # get image
         img_path, filename = self.samples[idx]
         img = self._load_image(img_path)
 
-        # Conditioning
-        condition = dict()
+        if self.transform:
+            img = self.transform(img)
 
-        if self.class_labels:
-            condition["class"] = self.class_labels[idx]
+        cond_dict: dict[str, Any] = {}
+        if hasattr(self, "conditions"):
+            for name, data_list in self.conditions.items():
+                val = data_list[idx]
+                # if it's a single column, collapse list -> scalar
+                if isinstance(val, (list, np.ndarray)) and len(val) == 1:
+                    val = val[0]
+                cond_dict[name] = val
 
-        if self.tabular_features:
-            tab_cond = self.tabular_features[idx]
-            condition["tabular"] = torch.tensor(tab_cond)
-
-        if self.cond_imgs: 
-            condition["image"] = img
-
-        return img, condition, filename
+        return img, cond_dict, filename
 
 
 class PairwiseHolographyImageFolder(BaseHolographyImageFolder):
     
-    def __init__(self, root, transform=None, labels=None, config=None, conditioning=None, verbose=False):
-        super().__init__(root, transform, labels, config, conditioning, verbose) 
+    def __init__(self, root, transform=None, labels=None, config=None, verbose=False):
+        super().__init__(root, transform, labels, config, verbose) 
 
 
     def load_annotations_from_csv(self):
